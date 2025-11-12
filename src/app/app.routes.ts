1126a3bd77a5d5cfb9ef88f713050402973c23c8
Daniel J <daniel.jonsson@consid.se>
2025-11-12 09:25:39 +0100

    the rest

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'gyms',
+    loadComponent: () =>
+      import('./gyms/gyms.component').then((m) => m.GymsComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((m) => m.AboutComponent),
+  },
+];
---
+++ src/app/gyms/gyms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gyms',
+  standalone: true,
+  imports: [],
+  templateUrl: './gyms.component.html',
+  styleUrl: './gyms.component.scss'
+})
+export class GymsComponent {
+
+}
